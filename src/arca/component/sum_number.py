0305e10616b83b4235262c8eb53e5808c9c32c37
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2020-07-21 22:22:51 +0100

    Fix component

--- src/arca/component/sum_number.py
+++ src/arca/component/sum_number.py
@@ -1,3 +1,4 @@
+from random import randint
 from typing import Any, Dict
 
 from arca import mark_component
@@ -18,9 +19,15 @@ def sum_num(num_1: int, num_2: int) -> int:
 
 
 @mark_component(
-    name="Create a dictionary",
+    name="Create even and odd number",
     param_names={"num": "Number to be added", "list_num": "List with numbers"},
-    unpack_output=["trevisani", "test", ("num", int)],
+    unpack_output=[("even", int), ("odd", int)],
 )
-def create_dict() -> Dict[str, Any]:
-    return {"trevisani": list(range(10)), "test": True, "num": 99}
+def create_even_odd_numbers():
+    """Create two integer numbers, one even and one odd number."""
+    rand_num = randint(0, 9999)
+    even_num = rand_num if rand_num % 2 == 0 else rand_num + 1
+
+    rand_num = randint(0, 9999)
+    odd_num = rand_num if rand_num % 2 == 1 else rand_num + 1
+    return {"even": even_num, "odd": odd_num}
